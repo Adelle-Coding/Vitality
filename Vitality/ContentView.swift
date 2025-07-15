3464df5f13d0155cd37df95842ae62da568a3a52
Aracely Garcia <Celyg374@gmail.com>
2025-07-15 13:19:00 -0400

    Main page+ encouragment and checklist page set up!

--- Vitality/ContentView.swift
+++ Vitality/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack{
+                Text("Vitality")
+                NavigationLink(destination: Checklist()) {
+                    Text("Checklist")
+                        
+                }
+                    
+                    NavigationLink(destination: Encouragement_.init()) {
+                        Text("Encouragement")
+                        
+                
+                }
+            }
         }
         .padding()
     }
---
+++ Vitality/Encouragement .swift
@@ -0,0 +1,18 @@
+//
+//  Encouragement .swift
+//  Vitality
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+struct Encouragement_: View {
+    var body: some View {
+        Text("Welcome to the Encouragement page ")
+    }
+}
+
+#Preview {
+    Encouragement_()
+}
