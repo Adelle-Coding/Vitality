a2ed14ff3e3a1253390b60bba9eeb44aec79dc65
Adelle Sahoo <adellesahoo@gmail.com>
2025-07-15 11:44:48 -0400

    Initial Commit

---
+++ Vitality/VitalityApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitalityApp.swift
+//  Vitality
+//
+//  Created by Scholar on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct VitalityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
