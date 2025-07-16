54ccd578592ae99ce54fe91351c326168b76009a
AraG-007 <celyg374@gmail.com>
2025-07-16 11:25:51 -0400

    Revert "Adelle dev"

--- Vitality/ContentView.swift
+++ Vitality/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         NavigationStack{
             VStack{
                 Text("Vitality")
-                NavigationLink(destination: ChecklistView()) {
+                NavigationLink(destination: Checklist()) {
                     Text("Checklist")
                         
                 }
